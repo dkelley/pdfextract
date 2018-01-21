ee2645af3309c21f82e9af72b3ddd5d6f5b23e79
Dan Kelley <dkelley@anexinet.com>
2018-01-21 12:55:09 -0500

    start of project

---
+++ PDFFormExtract/Services/IExtractService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDFFormExtract.Models;
+
+namespace PDFFormExtract.Services
+{
+    public interface IExtractService
+    {
+        //FormDimension ExtractData(byte[] file);
+        FormDefinition ExtractData(string filename, byte[] file);
+    }
+}
